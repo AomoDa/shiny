201a50a5268539641fecc9b5f84e203e3eb35484
Aomoda <528451599@qq.com>
2017-04-27 16:54:25 +0800

    Update server.r

--- server.r
+++ server.r
@@ -1,21 +1,29 @@
+data = read.csv("data/view(1).csv")
+source("helper.txt")
 shinyServer(function(input, output) {
+  
+  output$tb <-  renderTable({   
+    data[1:50,]
+    })
+ 
 
-  output$main_plot <- reactivePlot(width = 400, height = 300, function() {
-
-    hist(faithful$eruptions,
-      probability = TRUE,
-      breaks = as.numeric(input$n_breaks),
-      xlab = "Duration (minutes)",
-      main = "Geyser eruption duration")
-
-    if (input$individual_obs) {
-      rug(faithful$eruptions)
-    }
-
-    if (input$density) {
-      dens <- density(faithful$eruptions, adjust = input$bw_adjust)
-      lines(dens, col = "blue")
-    }
+  output$xx <- renderPlot({
+    plot(1:19)
+  })
+  aa <- 19
+  output$xxx <- renderPlot({
+    y <- as.numeric(input$a)
+    plot(1:y)
+  })
+  
+  output$cd <-  renderTable({   
 
   })
+  
+  output$desc <-  renderTable({  
+     set.seed(nchar(as.character(input$tt)))
+     a = unique(data$beer_style)
+     data.frame(Recommend = a[sample(1:length(a), sample(3:10,1))])
+  })
+  
 })
